7e11b32e209031cc5bbee0db0f49fec64e3f4d4f
Rajashekar <rajashekar@iipl.work>
2023-11-02 15:15:34 +0530

    bootstrap setup

---
+++ src/components/WeatherView.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <button class="btn btn-primary">AAAA</button>
+
+
+        <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button"
+            aria-controls="offcanvasExample">
+            Link with href
+        </a>
+
+        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
+            aria-controls="offcanvasExample">
+            Button with data-bs-target
+        </button>
+
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <div>
+                    Some text as a placeholder. In real life, you can have the elements you have chosen, like text, images,
+                    lists, etc.
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>  
+<script>
+
+export default {
+    name: 'WeatherView',
+    props: {
+        msg: String
+    }
+}
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
+  
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createApp(App).mount('#app')
